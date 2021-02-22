4630e18f0047a1fbefc941bc8018e2e5e207ecb6
daniel ouattara <daniel.ouattara@gmx.com>
2021-02-22 01:06:09 +0100

    styling connexion page

--- frontend_vuejs_vuex_vuerouter/src/App.vue
+++ frontend_vuejs_vuex_vuerouter/src/App.vue
@@ -4,51 +4,69 @@
    Our navbar looks more professional when using font-awesome-icon.
    We also make the navbar dynamically change by current User’s roles
    which are retrieved from Vuex Store state.-->
+
   <div id="app">
-    <nav class="navbar navbar-expand navbar-dark bg-dark">
-      <a href class="navbar-brand" @click.prevent>Groupomania</a>
-      <div class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <router-link to="/home" class="nav-link">
-            <font-awesome-icon icon="home" />Accueil
-          </router-link>
-        </li>
-        <li v-if="showAdminBoard" class="nav-item">
-          <router-link to="/admin" class="nav-link">Admin Board</router-link>
-        </li>
-        <!-- <li v-if="showModeratorBoard" class="nav-item">
-          <router-link to="/mod" class="nav-link">Moderator Board</router-link>
-        </li> -->
-        <li class="nav-item">
-          <router-link v-if="currentUser" to="/user" class="nav-link">User</router-link>
-        </li>
-      </div>
 
-      <div v-if="!currentUser" class="navbar-nav ml-auto">
-        <li class="nav-item">
-          <router-link to="/register" class="nav-link">
-            <font-awesome-icon icon="user-plus" />Inscription
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/login" class="nav-link">
-            <font-awesome-icon icon="sign-in-alt" />Connexion
-          </router-link>
-        </li>
-      </div>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+      <a href="#" class="navbar-brand" @click.prevent>
+        <img src="./assets/images/image_Groupomania/icon-left-font-monochrome-white.svg" style="width:200px" alt=" image logo Groupomania"/>
+      </a>
 
-      <div v-if="currentUser" class="navbar-nav ml-auto">
-        <li class="nav-item">
-          <router-link to="/profile" class="nav-link">
-            <font-awesome-icon icon="user" />
-            {{ currentUser.username }}
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href @click.prevent="logOut">
-            <font-awesome-icon icon="sign-out-alt" />Déconnexion
-          </a>
-        </li>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+          <div class="navbar-nav mr-auto">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                  <router-link to="/home" class="nav-link">
+                    <font-awesome-icon icon="home" />Accueil
+                  </router-link>
+                </li>
+                <li v-if="showAdminBoard" class="nav-item">
+                  <router-link to="/admin" class="nav-link">Admin Board</router-link>
+                </li>
+                <li class="nav-item">
+                  <router-link v-if="currentUser" to="/user" class="nav-link">User</router-link>
+                </li>
+            </ul>
+          </div>
+
+          <div v-if="!currentUser" class="navbar-nav ml-auto">
+
+            <ul class="navbar-nav">
+               <li class="nav-item">
+                  <router-link to="/register" class="nav-link">
+                    <font-awesome-icon icon="user-plus" />Inscription
+                  </router-link>
+               </li>
+               <li class="nav-item">
+                  <router-link to="/login" class="nav-link">
+                    <font-awesome-icon icon="sign-in-alt" />Connexion
+                  </router-link>
+               </li>
+            </ul>
+
+
+          </div>
+
+          <div v-if="currentUser" class="navbar-nav ml-auto">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+               <router-link to="/profile" class="nav-link">
+                  <font-awesome-icon icon="user" />
+                    {{ currentUser.username }}
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href @click.prevent="logOut">
+                  <font-awesome-icon icon="sign-out-alt" />Déconnexion
+                </a>
+              </li>
+            </ul>
+
+          </div>
       </div>
     </nav>
 
@@ -61,9 +79,11 @@
 <script>
 export default {
   computed: {
+
     currentUser() {
       return this.$store.state.auth.user;
     },
+
     showAdminBoard() {
       if (this.currentUser && this.currentUser.roles) {
         return this.currentUser.roles.includes('ROLE_ADMIN');
@@ -71,15 +91,16 @@ export default {
 
       return false;
     },
-    showModeratorBoard() {
-      if (this.currentUser && this.currentUser.roles) {
-        return this.currentUser.roles.includes('ROLE_MODERATOR');
-      }
+    // showModeratorBoard() {
+    //   if (this.currentUser && this.currentUser.roles) {
+    //     return this.currentUser.roles.includes('ROLE_MODERATOR');
+    //   }
 
-      return false;
-    }
+    //   return false;
+    // }
   },
   methods: {
+    
     logOut() {
       this.$store.dispatch('auth/logout');
       this.$router.push('/login');
--- frontend_vuejs_vuex_vuerouter/src/views/Login.vue
+++ frontend_vuejs_vuex_vuerouter/src/views/Login.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="col-md-12">
     <div class="card card-container">
-      <img
-        id="profile-img"
-        src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
-        class="profile-img-card"
-      />
+      <img id="profile-img" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" class="profile-img-card" alt="logo image profile" />
       <form name="form" @submit.prevent="handleLogin">
         <div class="form-group">
           <label for="username">Pseudo</label>
